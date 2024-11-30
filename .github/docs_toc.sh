8f42cecf05708535f0d88c94057d1ae9e778e905
Rizal Fadlullah <izzal@Rizals-MacBook-Air.local>
2024-12-01 01:01:30 +0700

    fix all

--- .github/docs_toc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-
-# Membuat header untuk README.md
-echo "# Dokumentasi Praktikum" > README.md
-find . -type f -name "*.pdf" | while read -r file; do
-    echo "- [$(basename "$file")]($file)" >> README.md
-done
